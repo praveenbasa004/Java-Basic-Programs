de4d4be9604fb55b1c8fcaa12852d5cb01924a42
praveenbasa004 <140961666+praveenbasa004@users.noreply.github.com>
2023-08-19 19:22:36 +0530

    Add files via upload

---
+++ pattern programs/basic pattern programs/Program1.java
@@ -0,0 +1,33 @@
+package Basic_Pattern_Programs;
+
+public class Program1 {
+
+	public static void main(String[] args)
+	{
+/*
+		for(int i=1;i<=5;i++)
+		{
+			System.out.print("*");
+			
+		}
+		
+		
+		//with while loop
+		int i=1;
+		while(i<=5)
+		{
+			System.out.print("*");
+			i++;
+		}
+*/		
+		
+		int i=1;
+		do
+		{
+			System.out.print("*");
+			i++;
+		}
+		while(i<=5);
+	}
+
+}
